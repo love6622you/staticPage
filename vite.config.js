761dbb702d8e15dd32915f25450cbef062fdd967
Chia <lovemaple0913@gmail.com>
2024-02-13 15:52:36 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslint()],
-  base: './staticPage/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'), // 設置 '@' 指向 'src' 目錄
